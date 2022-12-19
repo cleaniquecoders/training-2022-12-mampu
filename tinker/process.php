9e75d7262b1ffcc9bf6f9d84861e7405b2007540
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2022-12-20 00:42:09 +0800

    wip

--- tinker/process.php
+++ tinker/process.php
@@ -2,10 +2,10 @@
 
 use App\Jasper\PHPJasper;
 
-$input = storage_path('app/sample/Leaf_Grey.jasper');
+$input = storage_path('app/sample/Users.jasper');
 $output = storage_path('app/sample/outputs');
 $options = [
-    'format' => ['pdf', 'rtf'],
+    'format' => ['pdf'],
     'db_connection' => [
         'driver' => 'mysql',
         'username' => 'root',
@@ -14,7 +14,7 @@ $options = [
         'database' => 'jasper',
         'port' => 3306,
         'jdbc_driver' => 'mysql',
-        'jdbc_dir' => '/Users/nasrulhazim/Libraries/mysql-connector-j-8.0.31'
+        // 'jdbc_dir' => '/Users/nasrulhazim/Libraries/mysql-connector-j-8.0.31'
     ]
 ];
 
@@ -27,3 +27,5 @@ try {
 } catch (\Throwable $th) {
     echo $th->getMessage().PHP_EOL;
 }
+// cd vendor/geekcom/phpjasper/bin/jasperstarter/bin
+// ./jasperstarter process "/Users/nasrulhazim/Trainings/2022-12/mampu/jasper/storage/app/sample/Users.jasper" -o "/Users/nasrulhazim/Trainings/2022-12/mampu/jasper/storage/app/sample/outputs" -f pdf rtf -t mysql -u root -H 127.0.0.1 -n jasper --db-port 3306 --db-driver mysql --jdbc-dir /Users/nasrulhazim/Libraries/mysql-connector-j-8.0.31
