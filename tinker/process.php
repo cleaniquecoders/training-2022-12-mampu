187e4001ff74245ac22077c9f0cb84bef5ff7b33
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2022-12-20 00:29:33 +0800

    wip

--- tinker/process.php
+++ tinker/process.php
@@ -6,12 +6,24 @@ $input = storage_path('app/sample/Leaf_Grey.jasper');
 $output = storage_path('app/sample/outputs');
 $options = [
     'format' => ['pdf', 'rtf'],
+    'db_connection' => [
+        'driver' => 'mysql',
+        'username' => 'root',
+        // 'password' => '',
+        'host' => '127.0.0.1',
+        'database' => 'jasper',
+        'port' => 3306,
+        'jdbc_driver' => 'mysql',
+        'jdbc_dir' => '/Users/nasrulhazim/Libraries/mysql-connector-j-8.0.31'
+    ]
 ];
 
 $jasper = new PHPJasper;
 
 try {
-    $jasper->process($input, $output, $options)->execute();
+    // $jasper->process($input, $output, $options)->execute();
+    $x = $jasper->process($input, $output, $options)->output();
+    dd($x);
 } catch (\Throwable $th) {
     echo $th->getMessage().PHP_EOL;
 }
